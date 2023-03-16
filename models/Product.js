4785a5c7ab22044fe0c5055131719e13f5a4d4b2
Rakhshan <rakhshancoder@gmail.com>
2023-03-17 00:27:23 +0530

    update img type in Product models

--- models/Product.js
+++ models/Product.js
@@ -4,11 +4,12 @@ const ProductSchema = new mongoose.Schema(
     {
         title:{type:String, required: true, unique: true},
         desc:{type:String, required: true},
-        img:{type: String, required: true},
+        img:{type: Image, required: true},
         categories:{type: Array},
-        size:{type: String},
-        color:{type: String},
+        size:{type: Array},
+        color:{type: Array},
         price:{type: Number, required: true},
+        inStock:{type: Boolean, default: true},
     },
     //timestamps used to get current from server and it updates current time as well.
     {timestamps: true}
