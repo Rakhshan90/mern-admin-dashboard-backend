61d0e50f35d000ba9c4fd4b8f76a9495a838c225
Rakhshan <rakhshancoder@gmail.com>
2023-04-23 23:33:29 +0530

    apis are done except stripe does not working

--- models/User.js
+++ models/User.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 //creating our user schema
 const UserSchema = new mongoose.Schema(
     {
-        username:{type:String, required: true, unique: true},
-        email:{type:String, required: true, unique: true},
-        password:{type: String, required: true},
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
         //users cannot be an admin
-        isAdmin:{type: Boolean, default: false},
+        isAdmin: { type: Boolean, default: false },
+        img: { type: String }
     },
+
     //timestamps used to create current date and time as well as updates.
-    {timestamps: true}
+    { timestamps: true }
 );
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,7 @@ const Order = require('../models/Order');
 const {verifyTokenAndAuthorization, verifyTokenAndAdmin, verifyToken} = require('./verifyToken');
 
 
-//CREATE 
+//CREATE
 router.post('/', verifyToken, async(req, res)=>{
     const newOrder = new Order(req.body);
     
@@ -72,25 +72,33 @@ router.get("/",verifyTokenAndAdmin ,async(req, res)=>{
 
 //MONTHLY INCOME
 router.get('/income', verifyTokenAndAdmin, async(req, res)=>{
+    const productId = req.query.pid;
     const date = new Date();
     const lastMonth = new Date(date.setMonth(date.getMonth()-1));
     const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth()-1));
 
-    try{
+    try {
         const income = await Order.aggregate([
-            {$match : {createdAt: {$gte: previousMonth} } },
-            {
-                $project:{
-                    month:{$month: "$createdAt"},
-                    sales: "$amount",
-                },
+          {
+            $match: {
+              createdAt: { $gte: previousMonth },
+              ...(productId && {
+                products: { $elemMatch: { productId } },
+              }),
             },
-            {
-                $group: {
-                    _id: "$month",
-                    total : {$sum: "$sales"},
-                },
+          },
+          {
+            $project: {
+              month: { $month: "$createdAt" },
+              sales: "$amount",
             },
+          },
+          {
+            $group: {
+              _id: "$month",
+              total: { $sum: "$sales" },
+            },
+          },
         ]);
         res.status(200).json(income);
     }
